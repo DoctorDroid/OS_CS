946f37df7d539f1cc50b8ca1fae00079d5d3ae34
Joanne Middour <64432505+jmmiddour@users.noreply.github.com>
2020-12-11 19:27:50 -0500

    Completed first-pass solution

--- 05-Algorithms/moving_zeroes/moving_zeroes.py
+++ 05-Algorithms/moving_zeroes/moving_zeroes.py
@@ -2,11 +2,15 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+
+
+# First-pass solution (make it work!)
 def moving_zeroes(arr):
-    # Your code here
-
-    pass
-
+    for i in arr:  # Iterate through the values in array
+        if i == 0:  # If the value is 0
+            arr.remove(i)  # Remove the 0
+            arr.append(0)  # Add the 0 to the end of the array
+    return arr  # Close the loop by returning the new array
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
