dfae276351b4c7a9789b1f10cf06c8e1df86544a
Joanne Middour <64432505+jmmiddour@users.noreply.github.com>
2020-12-11 19:18:12 -0500

    Completed first-pass solution

--- 05-Algorithms/single_number/single_number.py
+++ 05-Algorithms/single_number/single_number.py
@@ -2,10 +2,20 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-def single_number(arr):
-    # Your code here
 
-    pass
+
+# First-pass solution (just make it work):
+def single_number(arr):
+    stack = {}  # Need to create an empty dictionary to store pairs
+    for char in arr:  # Iterate through the array
+        if char in stack:  # Iterate through the stack to match values
+            stack[char] += 1  # If values match increase stack value by 1
+        else:
+            stack[char] = 1  # If values do not match the stack value is just 1
+
+    for key, val in stack.items():  # Get the keys and values from the stack
+        if val == 1:  # If the value is 1
+            return key  # Return the key value
 
 
 if __name__ == '__main__':
